eab13595f6fadd72a1f9813e1e43b50226882a59
thanhtuan24 <thanhtuan2494@gmail.com>
2023-06-23 23:21:56 +0000

    update

--- react-library/src/Navbar/Navbar.tsx
+++ react-library/src/Navbar/Navbar.tsx
@@ -1,5 +1,20 @@
 export const Navbar = () => {
      return ( 
-     <nav className="navbar"></nav>
+     <nav className="navbar navbar-expand navbar-light">
+        <div className="container-fluid">
+            <span className="navbar-brand">Book store</span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown"
+             aria-controls="navbarDropdown" aria-expanded="false" aria-label="Collapse Navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarDropDown">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">HOME</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+     </nav>
      );
 };
